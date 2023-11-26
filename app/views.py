04a70d5ca51a69ed5858a9cd17e5d768960d2163
Anthony <ajs34@mail.aub.edu>
2023-11-26 11:51:34 +0100

    Offloaded getting unique values of journal names and sorting them to the DB? I guess so. Anyway, it is just more elegant.

--- app/views.py
+++ app/views.py
@@ -7,10 +7,8 @@ from app.models import PubmedArticle
 # Create your views here.
 
 def start_page(request):
-    pubmedarticle_list = PubmedArticle.objects.all()
-    journal_list = list(set([x.journal_title for x in pubmedarticle_list]))
-    journal_list.sort()
-    journal_list = [(i + 1, journal_list[i]) for i in range(len(journal_list))]
+    journal_list = PubmedArticle.objects.order_by('journal_title').values('journal_title').distinct()
+    journal_list = [(i + 1, journal_list[i]['journal_title']) for i in range(len(journal_list))]
     paginator = Paginator(journal_list, 200)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
