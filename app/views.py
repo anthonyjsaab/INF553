9ea3eafc3ad0fb05cfbb5bcdbb94b679c3e36a21
Anthony <ajs34@mail.aub.edu>
2023-11-28 10:20:39 +0100

    Minor refactoring

--- app/views.py
+++ app/views.py
@@ -4,15 +4,13 @@ from django.shortcuts import render
 from app.models import *
 
 
-# Create your views here.
-
 def start_page(request):
     journal_list = PubmedArticle.objects.order_by('journal_title').values('journal_title').distinct()
     journal_list = [(i + 1, journal_list[i]['journal_title']) for i in range(len(journal_list))]
     paginator = Paginator(journal_list, request.GET.get("size", 200))
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-    return render(request, "journals_list.html", {"page_obj": page_obj})
+    page_obj = paginator.get_page(request.GET.get("page"))
+    context = {"page_obj": page_obj, "size": request.GET.get("size", 200)}
+    return render(request, "journals_list.html", context)
 
 
 def journal_page(request, journal_title):
@@ -21,9 +19,9 @@ def journal_page(request, journal_title):
     authors_list = [(i + 1, authors_list[i].author_name, authors_list[i].author_id) for i in
                     range(len(authors_list))]
     paginator = Paginator(authors_list, request.GET.get("size", 200))
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-    return render(request, "authors_list.html", {"page_obj": page_obj, "journal_title": journal_title})
+    page_obj = paginator.get_page(request.GET.get("page"))
+    context = {"page_obj": page_obj, "size": request.GET.get("size", 200), "journal_title": journal_title}
+    return render(request, "authors_list.html", context)
 
 
 def author_page(request, author_id):
